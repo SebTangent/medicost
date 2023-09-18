6c0237535dd69fa4c62a2a52ca6dacb7d57ba9d2
SebYanuardy <144196382+SebTangent@users.noreply.github.com>
2023-09-18 16:16:33 -0400

    v1.5
    Finished The Nav Bar

--- medicost/src/nav.js
+++ medicost/src/nav.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import "./nav.css"
-function nav() {
+import React, { useState } from 'react';
+import "./nav.css";
+
+function Nav() {
+  const [showSideNav, setShowSideNav] = useState(false);
+
+  const toggleSideNav = () => {
+    setShowSideNav(!showSideNav);
+  };
+
   return (
-    <div className = "nav"> 
-    
-    
-    
-    
-    
-    
+    <div className="nav">
+      <div className="nav_background">
+        <div className="navContents">
+          <div className="navIcon" onClick={toggleSideNav}>
+            <h1>MEDICOST</h1>
+          </div>
+          <div className={`nav__links ${showSideNav ? 'show' : ''}`}>
+            <a href="#">Cost Estimator</a>
+            <a href="#">Find Location</a>
+            <a href="#">Compare Prices</a>
+            <a href="#">My Account</a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default nav
+export default Nav;
--- medicost/src/screens/homeScreen.js
+++ medicost/src/screens/homeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./homeScreen.css";
+import Nav from "../nav.js"
 
 function homeScreen() {
   return (
     <div className ="homeScreen">
+        <Nav />
 
-        <h1> Welcome to MediCost</h1>
+        <h1> Welcome to MediCost </h1>
 
 
 
